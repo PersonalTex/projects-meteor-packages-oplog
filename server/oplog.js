e4a9fce4e789964121e96311c6b5843aaec9348d
Paolo tessari <personal.tex@gmail.com>
2015-12-27 22:17:43 +0100

    Initial commit

--- server/oplog.js
+++ server/oplog.js
@@ -8,55 +8,54 @@ OpLogEvents = function(uri, filter, options) {
 }
 
 OpLogEvents.prototype.run = function() {
-       try {
-            var self = this;
+    try {
+        var self = this;
 
-            var oplog = MongoOplog(self.uri, {ns: self.filter}).tail();
+        var oplog = MongoOplog(self.uri, {ns: self.filter}).tail();
 
-            oplog.on('op', function (data) {
-                //console.log(data);
-            });
+        oplog.on('op', function (data) {
+            //console.log(data);
+        });
 
-            oplog.on('insert', function (doc) {
-                //console.log(doc.op);
-                console.log(doc.op);
-                self.insert(doc);
-            });
+        oplog.on('insert', function (doc) {
+            //console.log(doc.op);
+            console.log(doc.op);
+            self.insert(doc);
+        });
 
-            oplog.on('update', function (doc) {
-                console.log(doc.op);
-                self.update(doc);
+        oplog.on('update', function (doc) {
+            console.log(doc.op);
+            self.update(doc);
 
-            });
+        });
 
-            oplog.on('delete', function (doc) {
-                console.log(doc.op);
-                self.delete(doc);
-            });
+        oplog.on('delete', function (doc) {
+            console.log(doc.op);
+            self.delete(doc);
+        });
 
-            oplog.on('error', function (error) {
-                console.log(error);
-                throw error;
-            });
-
-            oplog.on('end', function () {
-                console.log('Stream ended');
-            });
-
-            oplog.stop(function () {
-                console.log('server stopped');
-            });
-
-            oplog.on('tail', function () {
-                console.log('tail');
-            });
-        }
-        catch (error) {
-            console.log("OpLogEvents creation failed")
+        oplog.on('error', function (error) {
+            console.log(error);
             throw error;
+        });
 
-        }
+        oplog.on('end', function () {
+            console.log('Stream ended');
+        });
 
+        oplog.stop(function () {
+            console.log('server stopped');
+        });
+
+        oplog.on('tail', function () {
+            console.log('tail');
+        });
+    }
+    catch (error) {
+        console.log("OpLogEvents creation failed")
+        throw error;
+
+    }
 };
 
 
@@ -86,11 +85,13 @@ OpLogWrite.prototype = Object.create(OpLogEvents.prototype);
 
 
 OpLogWrite.prototype.insert = function(doc) {
-
+    var future = new Future();
     console.log('insert ' + doc.o__id.toString());
     var sql = this.options.commandManager.prepareInsert(this.getCollectionName(doc), doc);
     console.log(sql);
-}
+
+
+}.future()
 
 OpLogWrite.prototype.update = function(doc) {
     console.log('update '+doc.o2._id.toString());
