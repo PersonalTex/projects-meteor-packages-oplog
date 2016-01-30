e152de6ebaa61e03c662216e7d1309bc86d60bfd
Paolo tessari <personal.tex@gmail.com>
2016-01-30 16:44:34 +0100

    manage setInterval to process syncdblog collection for each valid document write it in related legacy table

--- server/oplog.js
+++ server/oplog.js
@@ -6,16 +6,20 @@ var Future = Npm.require('fibers/future');
  OpLogEvents
  */
 
+var idProcessSyncDbLog
+
 OpLogEvents = function (uri, filter) {
     this.uri = uri;
     this.filter = filter;
-
+    this.intervalId = -1;
 };
 
 
 OpLogEvents.prototype.start = function () {
     try {
         var self = this;
+
+        idProcessSyncDbLog = setInterval(processSyncDbLog, 5000, self.syncdblog, self.connection);
 
         var oplog = MongoOplog(self.uri, {ns: self.filter}).tail();
 
@@ -100,13 +104,21 @@ OpLogWrite.prototype.writeRecord = function (doc, op) {
         var cmdMgr = new OpSequelizeCommandManager(self.connection, self.dbTables);
 
 
+        /*
         cmdMgr.ee.on('beforeExecSql', function (tableName, sql, doc, action) {
             self.writeDbLog(tableName, sql, doc, action);
         });
+         */
 
+        var sql = cmdMgr.prepareSql(tableName, doc, op).wait();
+        var record = cmdMgr.prepareRecord(tableName, doc, op).wait();
+        self.writeDbLog(tableName, sql, record, op).wait();
+
+
+        /*
         sql = cmdMgr.prepareSql(tableName, doc, op).wait();
         future.return(sql != '' ? cmdMgr.execSql(sql, tableName, doc, op).wait() : true);
-
+         */
 
         return future.wait();
 
@@ -124,18 +136,43 @@ OpLogWrite.prototype.writeDbLog = function (tableName, sql, doc, action) {
 
     var self = this;
 
-    //var future = new Future;
-
+    var future = new Future;
+    console.log('writeDbLog');
     self.syncdblog.insert({
         coll: tableName,
         oper: action,
         data: doc,
         command: sql,
+        status: "",
         created: {ts: new Date(), app: "integrator"}
     });
 
-    //future.return(true);
+    future.return(true);
 
 
-    //return future.wait();
-}//.future();
+    return future.wait();
+}.future();
+
+
+var processSyncDbLog = function (syncdblog, connection) {
+    var self = this;
+
+    var future = new Future;
+
+    console.log("processSyncDbLog");
+    if (connection.dbInstance != null) {
+        var command = new SequelizeCommand(connection);
+        syncdblog.find({status: ""}, {sort: {"created.ts": 1}}).forEach(function (doc) {
+
+            var ret = command.execSql(doc.command, doc.data, doc.oper).wait();
+            syncdblog.update({_id: doc['_id']}, {$set: {status: ret ? "OK" : "KO"}});
+        });
+        future.return(true);
+    }
+    else {
+        future.return(false);
+        clearInterval(idProcessSyncDbLog);
+    }
+    return future.wait();
+}.future();
+
